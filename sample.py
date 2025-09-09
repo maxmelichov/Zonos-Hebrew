c5fb08f4eec0de9abe0fecdf73405f776a413ccd
maxmelichov <maxme006@gmail.com>
2025-09-09 11:42:15 +0300

    Add phonikud and phonikud-onnx dependencies; update sample for Hebrew phonemization

--- sample.py
+++ sample.py
@@ -3,16 +3,21 @@ import torchaudio
 from zonos.model import Zonos
 from zonos.conditioning import make_cond_dict
 from zonos.utils import DEFAULT_DEVICE as device
+from phonikud import phonemize
 
-# model = Zonos.from_pretrained("Zyphra/Zonos-v0.1-hybrid", device=device)
-model = Zonos.from_pretrained("Zyphra/Zonos-v0.1-transformer", device=device)
+model = Zonos.from_pretrained("checkpoints/Zonos-v0.1-hybrid-Hebrew.pt", device=device)
+
+
 
 wav, sampling_rate = torchaudio.load("assets/exampleaudio.mp3")
 speaker = model.make_speaker_embedding(wav, sampling_rate)
 
 torch.manual_seed(421)
 
-cond_dict = make_cond_dict(text="Hello, world!", speaker=speaker, language="en-us")
+text = "שלום עולם"
+
+phonemes = phonemize(text, "he")
+cond_dict = make_cond_dict(text=phonemes, speaker=speaker, language="he")
 conditioning = model.prepare_conditioning(cond_dict)
 
 codes = model.generate(conditioning)
